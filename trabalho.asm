22d38e7ecffcd87292538e4afceb98564b7464b1
anamolinos <anamolinos52@gmail.com>
2025-02-24 14:30:32 -0300

    finalização e comentários.

--- trabalho.asm
+++ trabalho.asm
@@ -1,97 +1,177 @@
 segment .data
-a: dq 0
+; Variáveis de 8 bytes
+a: dq 2
+ind: dq 0
 cnt: dq 0
+cnt2: dq 0
 mini: dq 0
 maxi: dq 0
 min_pos: dq 0
 max_pos: dq 0
+fmt: dq "%lld ",10,0
 fmt_in: dq "%lld", 0
-fmt_out_max: dq "O maior numero esta na posicao %lld e eh %lld ", 10, 0
-fmt_out_min: dq "O menor numero esta na posicao %lld e eh %lld ", 10, 0
 
+; Strings
+fmt_out_max: db "O maior número está na posição %lld e é o %lld. ", 0
+fmt_out_min: db "O menor número está na posição %lld e é o %lld. ", 0
+
+fmt_even: db "Esse valor é par!", 10, 0
+fmt_odd: db "Esse valor é ímpar!", 10, 0
+
+; Arrays de tamanho 21 (?)
 segment .bss
-array resq 5
+array resq 21
+array2 resq 21
 
 segment .text
 global main
 extern printf
 extern scanf
-extern fflush
 
 main:
-    push RBP
+push RBP
 
-    ; Inicializa o array com 5 elementos
-    mov RCX, 0
-INPUT_ARRAY: 
-    cmp RCX, 5
-    jz DONE
-    mov [cnt], RCX
+mov RAX, 0 ; Armazena em [a]
+mov RCX, 0 ; Armazena em [cnt] (registrador do contador para utilizar no loop)
+mov RBX, 0 ; Armazena em [b]
 
-    ; Libera o buffer de saída antes de chamar scanf
-    mov RDI, 0
-    call fflush
+; Loop para preencher o array com as entradas do teclado
+INPUT_ARRAY:
+	cmp RCX, 5 ; Testa se o array já foi totalmente preenchido
+	jz DONE
+	mov [cnt], RCX
+	mov RAX, 0
+        ; Lê do teclado para a variável a
+        mov RDI, fmt_in
+	mov RSI, a
+	call scanf
+	; Põe o conteúdo de a e de cnt nos registradores respectivos e armazena o número lido nas duas arrays
+        mov RAX, [a]
+	mov RCX, [cnt]
+	mov [array+RCX*8], RAX
+	mov [array2+RCX*8], RAX
+	; Incrementa o contador e repete o loop de maneira recursiva
+	inc RCX	
+	jmp INPUT_ARRAY 
 
-    ; Lê o número do teclado
-    mov RAX, 0
-    mov RDI, fmt_in
-    mov RSI, a
-    call scanf
-
-    ; Armazena o número lido no array
-    mov RAX, [a]
-    mov RCX, [cnt]
-    mov [array+RCX*8], RAX
-    inc RCX
-    jmp INPUT_ARRAY 
-
+; Limpa os registradores
 DONE:
-    ; Inicializa mini e maxi com o primeiro elemento do array
-    mov RAX, [array]
-    mov [mini], RAX
-    mov [maxi], RAX
-    mov RCX, 0
+	mov RAX, 0
+	mov RCX, 0
+	mov RBX, 0	
 
-FIND_MIN_MAX:
-    cmp RCX, 5
-    jz PRINT_RESULTS
-    mov RAX, [array+RCX*8]
+; Ordenação por Bubble Sort
+
+; Loop externo, percorre cada elemento do array
+OUTER_LOOP:
+	cmp RCX, 5
+	jge END_LOOP ; Saí do loop caso o contador seja >=5, pois o array já foi todo percorrido
+	mov [cnt], RCX
+	mov RAX, [array+RCX*8]
+
+; Loop interno que compara cada elemento com seu vizinho direito
+INNER_LOOP:
+	inc RCX
+	cmp RCX, 5
+	jz OK ; Saí do loop caso o contador seja >=5, pois o array já foi todo percorrido
+	cmp RAX, [array+RCX*8] ; Compara RAX (array[i]) com o próximo elemento array[i+1]	
+	jle INNER_LOOP ; Comparação verdadeira (vizinhos em ordem crescente), o loop continua		
+	xchg RAX, [array+RCX*8] ; Comparação falsa (vizinhos em ordem decrescente), xchg realiza a troca entre os dois elementos
+	jmp INNER_LOOP
+
+; Finaliza o loop interno (é necessário recuperar o valor do contador do loop externo de [cnt]) e retorna ao loop externo
+OK:
+	mov RCX, [cnt]
+	mov [array+RCX*8], RAX
+	inc RCX
+	jmp OUTER_LOOP
+
+; Finaliza o loop externo
+END_LOOP:
+	mov RAX, 0
+	mov RBX, 0
+	mov RCX, 0
+	mov RAX, [array+RCX*8]
+	mov [mini], RAX ; Armazena o primeiro elemento do array ordenado (menor elemento)
+
+	mov RCX, 4	
+	mov RAX, [array+RCX*8]
+	mov [maxi], RAX ; Armazena o último elemento do array ordenado (maior elemento)
+	
+
+	mov RCX, 0
+	mov RAX, 0
+	mov RBX, 0	
+
+FIND_MAX:
+	cmp RCX, 5 ; Se chegou ao final do array ORIGINAL, significa que o maior elemento estava na ultima posição
+	mov [cnt], RCX	
+	jz PRINT_MAX
+	mov RAX, [maxi]
+	mov RBX, [array2+RCX*8]
+	cmp RAX, RBX ; Compara o elemento maxi com o elemento da array original
+	jz PRINT_MAX ; Se forem iguais, significa que encontrou sua posição e pode imprimi-la
+	mov RCX, [cnt]
+	inc RCX
+	jmp FIND_MAX
+
+PRINT_MAX:
+	; Imprime o valor máximo e sua posição
+        mov RAX, [maxi]
+	mov RDI, fmt_out_max
+	mov RSI, RCX
+	mov RDX, RAX
+	call printf
+        ; Atualiza RAX e RBX para que a label PRINT_PARITY cheque se o valor máximo é par
+	mov RCX, 0
+	mov RAX, [maxi]
+	mov RBX, 2
+        call PRINT_PARITY
     
-    ; Verifica se é o menor
-    cmp RAX, [mini]
-    jge NOT_MIN
-    mov [mini], RAX
-    mov [min_pos], RCX
-NOT_MIN:
 
-    ; Verifica se é o maior
-    cmp RAX, [maxi]
-    jle NOT_MAX
-    mov [maxi], RAX
-    mov [max_pos], RCX
-NOT_MAX:
+FIND_MIN:
+	cmp RCX, 5 ; Se chegou ao final do array ORIGINAL, significa que o maior elemento estava na ultima posição
+	mov [cnt], RCX	
+	jz PRINT_MIN
+	mov RAX, [mini]
+	mov RBX, [array2+RCX*8]
+	cmp RAX, RBX ; Compara o elemento maxi com o elemento da array original
+	jz PRINT_MIN ; Se forem iguais, significa que encontrou sua posição e pode imprimi-la
+	mov RCX, [cnt]
+	inc RCX
+	jmp FIND_MIN
 
-    inc RCX
-    jmp FIND_MIN_MAX
+PRINT_MIN:
+	mov RAX, [mini]
+	mov RDI, fmt_out_min
+	mov RSI, RCX
+	mov RDX, RAX
+	call printf
+        ; Atualiza RAX e RBX para que a label PRINT_PARITY cheque se o valor mínimo é par
+        mov RAX, [mini] 
+	mov RBX, 2
 
-PRINT_RESULTS:
-    ; Imprime o maior número
-    mov RDI, fmt_out_max
-    mov RSI, [max_pos]
-    mov RDX, [maxi]
-    call printf
-    mov RDI, 0
-    call fflush
+PRINT_PARITY:
+        mov RDX, 0    ; Zera RDX (RDX armazenará o resto da divisão)
+        idiv RBX      ; Divide RAX por RBX (RAX / 2), quociente em RAX, resto em RDX
+        cmp RDX, 1    ; Compara RDX (resto da divisão) com 1
+        jge ELSE      ; Se RDX >= 1 (resto diferente de 0), pula para ELSE
+        
+        mov RDI, fmt_even ; Define o formato para "par"
+        jmp LAST          ; Pula para LAST
 
-    ; Imprime o menor número
-    mov RDI, fmt_out_min
-    mov RSI, [min_pos]
-    mov RDX, [mini]
-    call printf
-    mov RDI, 0
-    call fflush
+        ELSE: 
+            mov RDI, fmt_odd  ; Define o formato para "ímpar"
+            jmp LAST          ; Pula para LAST
+            
+        LAST:
+            mov RAX , 0    ; Zera RAX e RBX antes de chamar printf
+            mov RBX, 0
+            call printf    ; Chama printf com os argumentos definidos
+        ret
+
 
 END:
-    mov RAX, 0
-    pop RBP
+	mov RAX, 0
+	pop RBP
 ret
